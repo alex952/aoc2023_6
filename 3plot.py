53e3fe3ef692d7d6c36da2d8a047fa0bbf87b768
Alejandro Navarro <alejandro.navarro@version1.com>
2024-02-26 12:00:38 +0100

    Adding some comments in the plot

--- 3plot.py
+++ 3plot.py
@@ -1,3 +1,16 @@
+# Mathematical approach we can represent the inequality such as
+# (t - x) * x > d
+# tx - x**2 > d
+# -x**2 + tx - d > 0
+
+# Classic quadratic formula
+# Coefficients would be:
+# a = -1
+# b = t
+# c = -d
+# Solve for x given that x needs to be integer the solutions
+# solving for > 0 would be ceil of + side and floor of - side
+
 t = 7
 d = 9
 
